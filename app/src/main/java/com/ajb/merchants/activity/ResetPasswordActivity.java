414427f1c44b7e9e065684928d4602ecc8ec9852
Yuven <iYuven@163.com>
2016-05-10 17:39:25 +0800

    新增重置密码页面；

---
+++ app/src/main/java/com/ajb/merchants/activity/ResetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.ajb.merchants.activity;
+
+import android.os.Bundle;
+
+import com.ajb.merchants.R;
+
+public class ResetPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
