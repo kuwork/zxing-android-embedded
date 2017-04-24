d7bc000c04a39033d4f6196b4474ce4fb120a3fa
Jerry <jerry535362936@126.com>
2017-04-24 10:27:01 +0800

    Support another way to handle the sweep code results in the CaptrueActivity

--- zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
+++ zxing-android-embedded/src/com/journeyapps/barcodescanner/CaptureManager.java
@@ -38,10 +38,10 @@ import java.util.Map;
  * Manages barcode scanning for a CaptureActivity. This class may be used to have a custom Activity
  * (e.g. with a customized look and feel, or a different superclass), but not the barcode scanning
  * process itself.
- *
+ * <p>
  * This is intended for an Activity that is dedicated to capturing a single barcode and returning
  * it via setResult(). For other use cases, use DefaultBarcodeScannerView or BarcodeView directly.
- *
+ * <p>
  * The following is managed by this class:
  * - Orientation lock
  * - InactivityTimer
@@ -54,6 +54,7 @@ public class CaptureManager {
     private static final String TAG = CaptureManager.class.getSimpleName();
 
     private static int cameraPermissionReqCode = 250;
+    private BarcodeCallback resultCallback;
 
     private Activity activity;
     private DecoratedBarcodeView barcodeView;
@@ -73,14 +74,16 @@ public class CaptureManager {
         public void barcodeResult(final BarcodeResult result) {
             barcodeView.pause();
             beepManager.playBeepSoundAndVibrate();
-
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    returnResult(result);
-                }
-            });
-
+            if (resultCallback == null) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        returnResult(result);
+                    }
+                });
+            } else {
+                resultCallback.barcodeResult(result);
+            }
         }
 
         @Override
@@ -111,7 +114,7 @@ public class CaptureManager {
         }
     };
 
-    public CaptureManager(Activity activity, DecoratedBarcodeView barcodeView) {
+    public CaptureManager(Activity activity, DecoratedBarcodeView barcodeView, BarcodeCallback callback) {
         this.activity = activity;
         this.barcodeView = barcodeView;
         barcodeView.getBarcodeView().addStateListener(stateListener);
@@ -127,12 +130,18 @@ public class CaptureManager {
         });
 
         beepManager = new BeepManager(activity);
+
+        this.resultCallback = callback;
+    }
+
+    public CaptureManager(Activity activity, DecoratedBarcodeView barcodeView) {
+        this(activity, barcodeView, null);
     }
 
     /**
      * Perform initialization, according to preferences set in the intent.
      *
-     * @param intent the intent containing the scanning preferences
+     * @param intent             the intent containing the scanning preferences
      * @param savedInstanceState saved state, containing orientation lock
      */
     public void initializeFromIntent(Intent intent, Bundle savedInstanceState) {
@@ -146,7 +155,7 @@ public class CaptureManager {
             this.orientationLock = savedInstanceState.getInt(SAVED_ORIENTATION_LOCK, ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
 
-        if(intent != null) {
+        if (intent != null) {
             // Only lock the orientation if it's not locked to something else yet
             boolean orientationLocked = intent.getBooleanExtra(Intents.Scan.ORIENTATION_LOCKED, true);
             if (orientationLocked) {
@@ -219,7 +228,7 @@ public class CaptureManager {
      * Call from Activity#onResume().
      */
     public void onResume() {
-        if(Build.VERSION.SDK_INT >= 23) {
+        if (Build.VERSION.SDK_INT >= 23) {
             openCameraWithPermission();
         } else {
             barcodeView.resume();
@@ -234,7 +243,7 @@ public class CaptureManager {
         if (ContextCompat.checkSelfPermission(this.activity, Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED) {
             barcodeView.resume();
-        } else if(!askedPermission) {
+        } else if (!askedPermission) {
             ActivityCompat.requestPermissions(this.activity,
                     new String[]{Manifest.permission.CAMERA},
                     cameraPermissionReqCode);
@@ -246,14 +255,15 @@ public class CaptureManager {
 
     /**
      * Call from Activity#onRequestPermissionsResult
-     * @param requestCode The request code passed in {@link android.support.v4.app.ActivityCompat#requestPermissions(Activity, String[], int)}.
-     * @param permissions The requested permissions.
+     *
+     * @param requestCode  The request code passed in {@link android.support.v4.app.ActivityCompat#requestPermissions(Activity, String[], int)}.
+     * @param permissions  The requested permissions.
      * @param grantResults The grant results for the corresponding permissions
-     *     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
-     *     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
+     *                     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
+     *                     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
      */
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if(requestCode == cameraPermissionReqCode) {
+        if (requestCode == cameraPermissionReqCode) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // permission was granted
                 barcodeView.resume();
@@ -291,7 +301,7 @@ public class CaptureManager {
     /**
      * Create a intent to return as the Activity result.
      *
-     * @param rawResult the BarcodeResult, must not be null.
+     * @param rawResult        the BarcodeResult, must not be null.
      * @param barcodeImagePath a path to an exported file of the Barcode Image, can be null.
      * @return the Intent
      */
